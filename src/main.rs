eb294debe576aa632923e95c9006c96843f6e490
dotzenith <sharmadanshu01@gmail.com>
2024-09-29 01:42:10 -0400

    quote wrapping and reorganization

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,22 @@
-mod images;
 mod api;
+mod images;
+mod utils;
 
-use viuer::{print, Config};
+use viuer::Config;
+pub const MAX_LINES_FOR_QUOTE: usize = 5;
+pub const HEIGHT: u8 = 10;
+pub const WIDTH: u8 = 20;
 
 fn main() {
-    let conf = Config {
-        width: Some(20),
-        height: Some(10),
-        restore_cursor: true,
+    let viuer_conf = Config {
+        width: Some(WIDTH as u32),
+        height: Some(HEIGHT as u32),
+        use_sixel: false, // Turning on sixel breaks wezterm for some reason
         absolute_offset: false,
         ..Default::default()
     };
-    let images = images::Images::new();
     let manager = api::APIManager::new().unwrap();
     let quotes = manager.random().unwrap();
-    let single = quotes.quotes.first().unwrap();
-    let image = images.fetch(&single.nation);
-
-    println!("");
-    print(&image, &conf).expect("Image printing failed.");
-    println!("");
-    println!("");
-    println!("");
-    println!("");
-    println!("                         {}", single.quote);
-    println!("                         {}", single.character);
-    println!("");
-    println!("");
-    println!("");
-    println!("");
-    println!("");
+    let (single, width) = utils::get_quote_and_width(&quotes);
+    utils::print_image_and_quote(single, width, &viuer_conf);
 }
---
+++ src/utils.rs
@@ -0,0 +1,71 @@
+use crate::api::{Quotes, QuotesInner};
+
+use crossterm::{
+    cursor::{RestorePosition, SavePosition},
+    execute,
+};
+use viuer::{print as print_image, Config};
+use crate::{MAX_LINES_FOR_QUOTE, WIDTH, HEIGHT};
+use crate::images::Images;
+
+pub fn print_image_and_quote(
+    quote: &QuotesInner,
+    width: usize,
+    viuer_conf: &Config,
+) {
+    let mut stdout = std::io::stdout();
+    let images = Images::new();
+    let quotes = quote_to_vec(&quote.quote, width);
+
+    println!(""); // First blank line
+
+    //////////////////////////////////////////////////////////////////
+    execute!(&mut stdout, SavePosition).unwrap();
+
+    print!("\n\n");
+    let mut lines_printed = 2;
+    for line in quotes.iter() {
+        println!("{}{}", " ".repeat(WIDTH as usize + 5), line);
+        lines_printed += 1;
+    }
+    println!("{}{}", " ".repeat(WIDTH as usize + 5), quote.character);
+    lines_printed += 1;
+
+    for _ in 0..HEIGHT - lines_printed {
+        println!("");
+    }
+
+    execute!(&mut stdout, RestorePosition).unwrap();
+    //////////////////////////////////////////////////////////////////
+
+    //////////////////////////////////////////////////////////////////
+    let image = images.fetch(&quote.nation);
+    print_image(&image, viuer_conf).expect("Image printing failed.");
+    //////////////////////////////////////////////////////////////////
+
+    println!(""); // Last blank line
+}
+
+pub fn get_quote_and_width(quotes: &Quotes) -> (&QuotesInner, usize) {
+    let term_width = term_size::dimensions().map(|(w, _)| w);
+    let default_width = Some(80);
+    let width = term_width.or(default_width).unwrap();
+
+    for quote in quotes.quotes.iter() {
+        if (quote.quote.len() / width) <= MAX_LINES_FOR_QUOTE {
+            return (quote, width);
+        }
+    }
+    (quotes.quotes.first().unwrap(), width)
+}
+
+fn quote_to_vec(quote: &str, width: usize) -> Vec<String> {
+    if width < 25 {
+        return vec![];
+    }
+
+    textwrap::wrap(&quote, width - 25)
+        .iter()
+        .map(|line| line.to_string())
+        .collect()
+}
